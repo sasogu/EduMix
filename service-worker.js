f306dd870ad7f0113c469cf003f89109c8e20807
Chuwi <chuwi@adsf.es>
2025-09-20 13:45:59 +0200

    109

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'edumix-cache-v1.0.8';
+const CACHE_NAME = 'edumix-cache-v1.0.9';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css',
   './app.js',
-  './app.js?v=1.0.8',
+  './app.js?v=1.0.9',
   './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png',
